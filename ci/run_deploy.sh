04a6d7455785704097e11bf672c7fa7d10666916
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-03-10 19:11:45 -0300

    Travis init

---
+++ ci/run_deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "Check if we should deploy and imagine we are going to the playstore :)"
---
+++ ci/run_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+STDOUT_FILTERS=$TRAVIS_BUILD_DIR'/travis/stdout_filters.sed'
+CYAN='\033[0;36m'
+NO_COLOR='\033[0m'
+
+echo -e "${CYAN}Running: ./gradlew $TEST_SUITE ${NO_COLOR}-> with filters"
+./gradlew $TEST_SUITE | sed -f $STDOUT_FILTERS
+
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
+    echo "Error while running ./gradlew $TEST_SUITE"
+    exit 1
+fi
