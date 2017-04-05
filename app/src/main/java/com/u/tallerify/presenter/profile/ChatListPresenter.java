c5f0ee6b08159d29ce3ac97cd4dfc7b48db3b618
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-04-05 03:22:24 -0300

    Mergeable

--- app/src/main/java/com/u/tallerify/presenter/profile/ChatListPresenter.java
+++ app/src/main/java/com/u/tallerify/presenter/profile/ChatListPresenter.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewStub;
 import android.widget.TextView;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
