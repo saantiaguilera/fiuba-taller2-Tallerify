39b7cdcbbc6a55c8086a7d983cfdba67b9413ff8
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-04-05 21:02:19 -0300

    Consume user data

--- app/src/main/java/com/u/tallerify/presenter/profile/ProfileUserInfoPresenter.java
+++ app/src/main/java/com/u/tallerify/presenter/profile/ProfileUserInfoPresenter.java
@@ -2,7 +2,13 @@ package com.u.tallerify.presenter.profile;
 
 import android.support.annotation.NonNull;
 import com.u.tallerify.contract.profile.ProfileUserInfoContract;
+import com.u.tallerify.model.entity.User;
+import com.u.tallerify.networking.ReactiveModel;
+import com.u.tallerify.networking.interactor.me.MeInteractor;
 import com.u.tallerify.presenter.Presenter;
+import javax.annotation.Nullable;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by saguilera on 3/30/17.
@@ -10,12 +16,38 @@ import com.u.tallerify.presenter.Presenter;
 public class ProfileUserInfoPresenter extends Presenter<ProfileUserInfoContract.View>
         implements ProfileUserInfoContract.Presenter {
 
+    @Nullable User me;
+
+    public ProfileUserInfoPresenter() {
+        MeInteractor.instance().observeUser()
+            .observeOn(Schedulers.io())
+            .subscribeOn(Schedulers.io())
+            .compose(this.<ReactiveModel<User>>bindToLifecycle())
+            .subscribe(new Action1<ReactiveModel<User>>() {
+                @Override
+                public void call(final ReactiveModel<User> rxModel) {
+                    if (rxModel.model() != null && !rxModel.hasError()) {
+                        me = rxModel.model();
+
+                        if (isAttached()) {
+                            requestView();
+                        }
+                    }
+                }
+            })
+    }
+
     @Override
     protected void onAttach(@NonNull final ProfileUserInfoContract.View view) {
-        view.setUserImage("http://placehold.it/320x320");
         view.setUserLastLocation("FIUBA - Paseo Colon 850");
-        view.setUserName("Santi Aguilera");
-        // TODO Remove mocks
+        if (me != null) {
+            setUser(view);
+        }
+    }
+
+    private void setUser(@NonNull ProfileUserInfoContract.View view) {
+        view.setUserName(me.name());
+        view.setUserImage(me.pictures().get(0));
     }
 
 }
