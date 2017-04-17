24440f2a40c8dc1f985bc5d23f20e404b6f1805d
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-04-17 00:35:01 -0300

    Custom dialog images

--- app/src/main/java/com/u/tallerify/presenter/abstracts/BaseDialogPresenter.java
+++ app/src/main/java/com/u/tallerify/presenter/abstracts/BaseDialogPresenter.java
@@ -2,11 +2,12 @@ package com.u.tallerify.presenter.abstracts;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.view.View;
 import com.u.tallerify.R;
 import com.u.tallerify.contract.abstracts.BaseDialogContract;
 import com.u.tallerify.presenter.Presenter;
+import rx.functions.Action1;
+import rx.subjects.PublishSubject;
 
 /**
  * Created by saguilera on 3/12/17.
@@ -17,12 +18,16 @@ public class BaseDialogPresenter extends Presenter<BaseDialogContract.View> impl
     private @NonNull String title;
 
     private @Nullable View content;
+    @Nullable PublishSubject<Void> imageClicksSubject;
+    private @Nullable String imageUrl;
 
     BaseDialogPresenter(@NonNull Builder builder) {
         super();
         this.severity = builder.severity;
         this.title = builder.title;
         this.content = builder.content;
+        this.imageUrl = builder.imageUrl;
+        this.imageClicksSubject = builder.imageClicksSubject;
     }
 
     @SuppressWarnings("ConstantConditions")
@@ -34,22 +39,39 @@ public class BaseDialogPresenter extends Presenter<BaseDialogContract.View> impl
             view.setContentView(content);
         }
 
+        if (imageClicksSubject != null) {
+            view.observeImageClicks()
+                .compose(this.<Void>bindToLifecycle())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(final Void aVoid) {
+                        imageClicksSubject.onNext(aVoid);
+                    }
+                });
+        }
+
         switch (severity) {
             case ERROR:
-                view.setSeverityImage(ContextCompat.getDrawable(getContext(), R.drawable.ic_cancel_black_36dp));
+                view.setSeverityImage(R.drawable.ic_cancel_black_36dp);
                 break;
             case WARNING:
-                view.setSeverityImage(ContextCompat.getDrawable(getContext(), R.drawable.ic_warning_black_36dp));
+                view.setSeverityImage(R.drawable.ic_warning_black_36dp);
                 break;
             case INFO:
-                view.setSeverityImage(ContextCompat.getDrawable(getContext(), R.drawable.ic_info_outline_black_36dp));
+                view.setSeverityImage(R.drawable.ic_info_outline_black_36dp);
+                break;
+            case CUSTOM:
+                if (imageUrl != null) {
+                    view.setSeverityImageUrl(imageUrl);
+                }
         }
     }
 
     public enum Severity {
         ERROR,
         WARNING,
-        INFO
+        INFO,
+        CUSTOM
     }
 
     public static class Builder {
@@ -57,6 +79,8 @@ public class BaseDialogPresenter extends Presenter<BaseDialogContract.View> impl
         @Nullable Severity severity;
         @Nullable String title;
         @Nullable View content;
+        @Nullable String imageUrl;
+        @Nullable PublishSubject<Void> imageClicksSubject;
 
         public Builder() {}
 
@@ -72,6 +96,16 @@ public class BaseDialogPresenter extends Presenter<BaseDialogContract.View> impl
 
         public @NonNull Builder content(@NonNull View content) {
             this.content = content;
+            return this;
+        }
+
+        public @NonNull Builder imageUrl(@Nullable String imageUrl) {
+            this.imageUrl = imageUrl;
+            return this;
+        }
+
+        public @NonNull Builder imageClicksSubject(@Nullable PublishSubject<Void> subject) {
+            this.imageClicksSubject = subject;
             return this;
         }
 
