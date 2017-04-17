1ba45343f3065f6ba0feb91ce5f4bf70111eae5d
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-04-17 20:08:10 -0300

    Native login implemented ??

--- app/src/main/java/com/u/tallerify/contract/login/LoginNativeContract.java
+++ app/src/main/java/com/u/tallerify/contract/login/LoginNativeContract.java
@@ -1,5 +1,6 @@
 package com.u.tallerify.contract.login;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import com.u.tallerify.contract.ContractPresenter;
 import com.u.tallerify.contract.ContractView;
@@ -11,7 +12,17 @@ import rx.Observable;
 
 public interface LoginNativeContract {
 
+    @NonNull String KEY_USERNAME = LoginNativeContract.class.getName() + "username";
+    @NonNull String KEY_PASSWORD = LoginNativeContract.class.getName() + "password";
+    @NonNull String KEY_FIRSTNAME = LoginNativeContract.class.getName() + "firstname";
+    @NonNull String KEY_LASTNAME = LoginNativeContract.class.getName() + "surname";
+    @NonNull String KEY_EMAIL = LoginNativeContract.class.getName() + "email";
+    @NonNull String KEY_BIRTHDAY = LoginNativeContract.class.getName() + "birthday";
+
     interface View extends ContractView {
+
+        @NonNull Observable<Bundle> observeLoginClicks();
+        @NonNull Observable<Bundle> observeSignUpClicks();
 
     }
 
--- app/src/main/java/com/u/tallerify/presenter/login/LoginNativeDialogPresenter.java
+++ app/src/main/java/com/u/tallerify/presenter/login/LoginNativeDialogPresenter.java
@@ -1,8 +1,16 @@
 package com.u.tallerify.presenter.login;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import com.u.tallerify.contract.login.LoginNativeContract;
+import com.u.tallerify.model.AccessToken;
+import com.u.tallerify.networking.interactor.Interactors;
+import com.u.tallerify.networking.interactor.credentials.CredentialsInteractor;
+import com.u.tallerify.networking.services.credentials.CredentialsService;
 import com.u.tallerify.presenter.Presenter;
+import com.u.tallerify.view.login.LoginNativeDialogView;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by saguilera on 4/13/17.
@@ -11,8 +19,44 @@ public class LoginNativeDialogPresenter extends Presenter<LoginNativeContract.Vi
         implements LoginNativeContract.Presenter {
 
     @Override
-    protected void onRender(@NonNull final LoginNativeContract.View view) {
+    protected void onAttach(@NonNull final LoginNativeContract.View view) {
+        super.onAttach(view);
 
+        view.observeLoginClicks()
+            .observeOn(Schedulers.computation())
+            .subscribeOn(Schedulers.computation())
+            .compose(this.<Bundle>bindToLifecycle())
+            .subscribe(new Action1<Bundle>() {
+                @Override
+                public void call(final Bundle bundle) {
+                    CredentialsInteractor.instance()
+                        .createWithNative(
+                            getContext(),
+                            new CredentialsService.CreateNativeForm(
+                                bundle.getString(LoginNativeContract.KEY_USERNAME),
+                                bundle.getString(LoginNativeContract.KEY_PASSWORD)
+                            )
+                        )
+                        .observeOn(Schedulers.io())
+                        .subscribeOn(Schedulers.io())
+                        .compose(LoginNativeDialogPresenter.this.<AccessToken>bindToLifecycle())
+                        .subscribe(Interactors.ACTION_NEXT, Interactors.ACTION_ERROR);
+                }
+            });
+
+        view.observeSignUpClicks()
+            .observeOn(Schedulers.computation())
+            .subscribeOn(Schedulers.computation())
+            .compose(this.<Bundle>bindToLifecycle())
+            .subscribe(new Action1<Bundle>() {
+                @Override
+                public void call(final Bundle bundle) {
+                    // TODO do signup and then login
+                }
+            });
     }
+
+    @Override
+    protected void onRender(@NonNull final LoginNativeContract.View view) {}
 
 }
