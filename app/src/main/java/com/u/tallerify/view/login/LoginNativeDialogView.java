439ecc9917f18f0f86475e11686447c21d15372c
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-04-18 22:53:20 -0300

    qt ui

--- app/src/main/java/com/u/tallerify/view/login/LoginNativeDialogView.java
+++ app/src/main/java/com/u/tallerify/view/login/LoginNativeDialogView.java
@@ -1,19 +1,22 @@
 package com.u.tallerify.view.login;
 
+import android.animation.LayoutTransition;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.ScrollView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.u.tallerify.R;
 import com.u.tallerify.contract.login.LoginNativeContract;
+import com.u.tallerify.utils.MetricsUtils;
+import com.u.tallerify.view.base.widget.InputTextView;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import rx.Observable;
 import rx.subjects.PublishSubject;
 
@@ -21,70 +24,74 @@ import rx.subjects.PublishSubject;
  * Created by saguilera on 4/13/17.
  */
 
-public class LoginNativeDialogView extends ScrollView
+public class LoginNativeDialogView extends LinearLayout
         implements LoginNativeContract.View {
 
-    @NonNull EditText userNameField;
-    @NonNull EditText passwordField;
+    @NonNull InputTextView userNameField;
+    @NonNull InputTextView passwordField;
 
     @NonNull ViewGroup signupExtrasContainer;
 
     @NonNull View actionButton;
-    @NonNull TextView signupVisibilityActionField;
+    @NonNull TextView swapActionVisibilityField;
 
-    @NonNull EditText nameField;
-    @NonNull EditText surnameField;
-    @NonNull EditText emailField;
-    @NonNull DatePicker birthdayField;
+    @NonNull InputTextView nameField;
+    @NonNull InputTextView surnameField;
+    @NonNull InputTextView emailField;
+    @NonNull InputTextView birthdayField;
 
     @NonNull PublishSubject<Bundle> signupSubject = PublishSubject.create();
     @NonNull PublishSubject<Bundle> loginSubject = PublishSubject.create();
-    @NonNull PublishSubject<Boolean> signupVisibilitySubject = PublishSubject.create();
+    @NonNull PublishSubject<Boolean> swapActionVisibilitySubject = PublishSubject.create();
+
+    @NonNull Calendar calendar = Calendar.getInstance();
 
     public LoginNativeDialogView(final Context context) {
         super(context);
 
         inflate(context, R.layout.view_login_native_dialog, this);
 
-        userNameField = (EditText) findViewById(R.id.view_login_native_dialog_username);
-        passwordField = (EditText) findViewById(R.id.view_login_native_dialog_password);
+        int width = MetricsUtils.getScreenPixelBounds(context).x;
+        width -= width/10;
 
-        nameField = (EditText) findViewById(R.id.view_login_native_dialog_name);
-        surnameField = (EditText) findViewById(R.id.view_login_native_dialog_surname);
-        emailField = (EditText) findViewById(R.id.view_login_native_dialog_mail);
-        birthdayField = (DatePicker) findViewById(R.id.view_login_native_dialog_birthday);
+        setOrientation(VERTICAL);
+        setLayoutTransition(new LayoutTransition());
+        setLayoutParams(new ViewGroup.LayoutParams(width,
+            ViewGroup.LayoutParams.MATCH_PARENT));
+
+        userNameField = (InputTextView) findViewById(R.id.view_login_native_dialog_username);
+        passwordField = (InputTextView) findViewById(R.id.view_login_native_dialog_password);
+
+        nameField = (InputTextView) findViewById(R.id.view_login_native_dialog_name);
+        surnameField = (InputTextView) findViewById(R.id.view_login_native_dialog_surname);
+        emailField = (InputTextView) findViewById(R.id.view_login_native_dialog_mail);
+        birthdayField = (InputTextView) findViewById(R.id.view_login_native_dialog_birthday);
 
         signupExtrasContainer = (ViewGroup) findViewById(R.id.view_login_native_dialog_signup_extras_container);
 
         actionButton = findViewById(R.id.view_login_native_dialog_action_button);
-        signupVisibilityActionField = (TextView) findViewById(R.id.view_login_native_dialog_signup_button);
+        swapActionVisibilityField = (TextView) findViewById(R.id.view_login_native_dialog_signup_button);
+
+        hideSignUp();
 
         actionButton.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(final View v) {
                 if (!isActionPerformable()) {
+                    markErrors();
                     return;
                 }
 
                 Bundle bundle = new Bundle();
-                bundle.putString(LoginNativeContract.KEY_USERNAME, userNameField.getText().toString());
-                bundle.putString(LoginNativeContract.KEY_PASSWORD, passwordField.getText().toString());
+                bundle.putString(LoginNativeContract.KEY_USERNAME, userNameField.getEditText().getText().toString());
+                bundle.putString(LoginNativeContract.KEY_PASSWORD, passwordField.getEditText().getText().toString());
 
                 if (isSignUp()) {
                     bundle.putString(LoginNativeContract.KEY_FIRSTNAME, nameField.getText().toString());
                     bundle.putString(LoginNativeContract.KEY_LASTNAME, surnameField.getText().toString());
                     bundle.putString(LoginNativeContract.KEY_EMAIL, emailField.getText().toString());
-
-                    int year = birthdayField.getYear();
-                    int month = birthdayField.getMonth();
-                    int day = birthdayField.getDayOfMonth();
-
-                    Calendar calendar = Calendar.getInstance();
-                    calendar.set(year, month, day);
-                    Date date = calendar.getTime();
-
-                    bundle.putSerializable(LoginNativeContract.KEY_BIRTHDAY, date);
+                    bundle.putSerializable(LoginNativeContract.KEY_BIRTHDAY, calendar.getTime());
 
                     signupSubject.onNext(bundle);
                 } else {
@@ -94,31 +101,93 @@ public class LoginNativeDialogView extends ScrollView
 
         });
 
-        signupVisibilityActionField.setOnClickListener(new OnClickListener() {
+        birthdayField.getEditText().setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(final View v, final boolean hasFocus) {
+                if (hasFocus) {
+                    new DatePickerDialog(
+                        getContext(),
+                        R.style.DatePickerDialogTheme,
+                        dateListener,
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)
+                    ).show();
+
+                    InputMethodManager imm = (InputMethodManager) getContext().getApplicationContext()
+                        .getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getWindowToken(), 0);
+
+                    requestFocus();
+                }
+            }
+        });
+
+        swapActionVisibilityField.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(final View v) {
-                if (isSignUp()) {
-                    signupVisibilityActionField.setText("Ya tengo cuenta!");
-                    signupExtrasContainer.setVisibility(View.GONE);
-                } else {
-                    signupVisibilityActionField.setText("Aun no tienes cuenta ? Create una !");
-                    signupExtrasContainer.setVisibility(View.VISIBLE);
-                }
-
-                signupVisibilitySubject.onNext(isSignUp());
+                swapActionVisibilityText();
+                swapActionVisibilitySubject.onNext(isSignUp());
             }
         });
     }
 
+    void hideSignUp() {
+        swapActionVisibilityField.setText("Aun no tienes cuenta ? Create una !");
+        signupExtrasContainer.setVisibility(View.GONE);
+    }
+
+    void showSignUp() {
+        swapActionVisibilityField.setText("Ya tengo cuenta!");
+        signupExtrasContainer.setVisibility(View.VISIBLE);
+    }
+
+    void swapActionVisibilityText() {
+        if (isSignUp()) {
+            hideSignUp();
+        } else {
+            showSignUp();
+        }
+    }
+
+    void markErrors() {
+        if (userNameField.getEditText().getText().toString().isEmpty()) {
+            userNameField.setError("Este campo es obligatorio!");
+        }
+
+        if (passwordField.getEditText().getText().toString().isEmpty()) {
+            passwordField.setError("Este campo es obligatorio!");
+        }
+
+        if (isSignUp()) {
+            if (nameField.getEditText().getText().toString().isEmpty()) {
+                nameField.setError("Este campo es obligatorio!");
+            }
+
+            if (surnameField.getEditText().getText().toString().isEmpty()) {
+                surnameField.setError("Este campo es obligatorio!");
+            }
+
+            if (emailField.getEditText().getText().toString().isEmpty()) {
+                emailField.setError("Este campo es obligatorio!");
+            }
+
+            if (birthdayField.getEditText().getText().toString().isEmpty()) {
+                birthdayField.setError("Este campo es obligatorio!");
+            }
+        }
+    }
+
     boolean isActionPerformable() {
         boolean can = true;
-        can &= !userNameField.getText().toString().isEmpty();
-        can &= !passwordField.getText().toString().isEmpty();
+        can &= !userNameField.getEditText().getText().toString().isEmpty();
+        can &= !passwordField.getEditText().getText().toString().isEmpty();
 
         if (isSignUp()) {
             can &= !nameField.getText().toString().isEmpty();
             can &= !surnameField.getText().toString().isEmpty();
             can &= !emailField.getText().toString().isEmpty();
+            can &= !birthdayField.getText().toString().isEmpty();
             return can;
         } else {
             return can;
@@ -144,6 +213,20 @@ public class LoginNativeDialogView extends ScrollView
     @NonNull
     @Override
     public Observable<Boolean> observeSignUpVisibilityChanges() {
-        return signupVisibilitySubject;
+        return swapActionVisibilitySubject;
     }
+
+    DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                int dayOfMonth) {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, monthOfYear);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            birthdayField.setText(new SimpleDateFormat("yyyy/mm/dd").format(calendar.getTime()));
+        }
+
+    };
+
 }
